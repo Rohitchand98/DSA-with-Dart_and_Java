69f3b72f9c46c5bb30bc98b8b5f62af46f116717
Rohitchand98 <rs486345@gmail.com>
2026-04-26 01:59:16 +0530

    array in Java

--- Learn Java26/Array.java
+++ Learn Java26/Array.java
@@ -1,3 +1,24 @@
 public class Array {
-    
+    void main() {
+        //Array should have limited blocks of element numbers like new string[anynumbers];
+        String[] arrayplayer= new String[3];
+        arrayplayer[0]="Ronaldo";
+        arrayplayer[1]="Messi";
+        arrayplayer[2]="Lewa";
+
+        //or in direct add of elements in array
+        String[] arrayplayers= {"tom","jerry","dog"};
+        //or loop 
+        String[] newarray= new String[3];
+        for(int i=0; i<arrayplayer.length; i++){
+            newarray[i]=arrayplayer[i];
+        }
+
+        //How to Print arrays 
+        IO.println(String.join(" ", arrayplayer));
+        for(int i =0; i<arrayplayer.length; i++){
+            IO.println(arrayplayers[i]);
+        }
+        IO.print("This is one word : "+String.join(" ", newarray[2]));
+    }
 }
