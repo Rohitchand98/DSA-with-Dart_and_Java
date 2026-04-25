ba73ca272b8b2b1343c9cd28e3f35f0f9cda404a
Rohitchand98 <rs486345@gmail.com>
2026-04-25 23:41:16 +0530

    New Basic Java

---
+++ Learn Java26/BasicJavaIO.java
@@ -0,0 +1,10 @@
+public class BasicJavaIO {
+    void main(){
+        IO.println("Hello New Java");
+        var java=IO.readln("Enter anything Here: ");
+        if(java.matches("-?\\d+")){
+            IO.println("Your number is "+java);
+        }else{
+        IO.println(java+ " Nice to Meet you");
+    }}
+}
