9deb71182e5fc3cbf6b8547012e447ea6b2e6e2e
Rohitchand98 <rs486345@gmail.com>
2026-04-27 23:55:39 +0530

    Old switch for java

---
+++ Learn Java26/switch_with_new.java
@@ -0,0 +1,14 @@
+
+
+public class switch_with_new {
+    //lets take example of OLD SWITCH
+    void main() {
+        int day=1;
+            switch(day){
+                case 1: 
+                System.out.println("Monday");
+                break;
+        }
+    }
+    
+}
