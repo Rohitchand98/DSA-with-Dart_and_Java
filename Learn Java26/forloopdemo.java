5d0d6e4b830a9c5c45e88980f2d6a7676be69cc6
Rohitchand98 <rs486345@gmail.com>
2026-05-01 23:53:58 +0530

    new feature for FORLOOP to implement more efficiently in Java

--- Learn Java26/forloopdemo.java
+++ Learn Java26/forloopdemo.java
@@ -13,8 +13,22 @@ public class forloopdemo {
         int sizeoflist= listofname.size();
         for(int i=0; i<sizeoflist;i++){
             IO.print(listofname.get(i)+" ");
-        }
+        }IO.println();
         
+        //Latest way to print a list or array was using forEach
+        IO.println("1. Using keyword 'ForEach'");
+        listofname.forEach(i-> IO.print(i+" "));
 
+        //two variable in for loop
+        IO.println("");
+        for (String elem : listofname) {
+            IO.print(elem+" ");
+        }IO.println();
+
+        //now try it in array(forEach doesnot work in array)
+        int[] array={1,2,3};
+        for (Object elem : array) {
+            IO.print(elem+" ");
+        }
     }
 }
