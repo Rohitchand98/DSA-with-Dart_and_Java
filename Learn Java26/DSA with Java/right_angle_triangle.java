c039826f7b544fb2ee35c82fb34adf82aa90f69f
Rohitchand98 <rs486345@gmail.com>
2026-05-03 17:03:31 +0530

    mirror triangle

--- Learn Java26/DSA with Java/right_angle_triangle.java
+++ Learn Java26/DSA with Java/right_angle_triangle.java
@@ -29,6 +29,16 @@ public class right_angle_triangle {
             }
             IO.println("");
         }IO.println("Hollow Right angle traingle\n");
+
+        //tilted right angle triangle
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n-i; j++) {
+                IO.print("   ");
+            }
+            for (int k = 0; k <= i; k++) {
+                IO.print(" * ");
+            }IO.println();
+        }
     
     }
 
