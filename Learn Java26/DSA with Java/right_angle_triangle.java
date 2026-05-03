cfa1c121ed4f687e1b818617a8c14da1b9cf56d3
Rohitchand98 <rs486345@gmail.com>
2026-05-03 16:23:47 +0530

    inverse of right angle traingle

--- Learn Java26/DSA with Java/right_angle_triangle.java
+++ Learn Java26/DSA with Java/right_angle_triangle.java
@@ -2,11 +2,22 @@
 public class right_angle_triangle {
     void main(){
         int n=5;
+        //its right angle traingle
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 IO.print("* ");
             }
             IO.println(" ");
+        }IO.println("Right angle traingle\n");
+
+        //inverse right angle traingle
+
+        for (int i = n; i >= 0; i--) {
+            for (int j = i; j >= 0; j--){
+                IO.print("* ");
+            }IO.println("");
+            }
+            IO.println("This is Inverse of right angle traingle\n");
+        
         }
     }
-}
