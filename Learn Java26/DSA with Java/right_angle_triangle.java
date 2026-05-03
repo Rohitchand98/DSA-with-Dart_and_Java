910a91e57e37ed1914df1cf94ce3d3c3c7a29162
Rohitchand98 <rs486345@gmail.com>
2026-05-03 16:55:24 +0530

    Hollow right angle traingle

--- Learn Java26/DSA with Java/right_angle_triangle.java
+++ Learn Java26/DSA with Java/right_angle_triangle.java
@@ -4,20 +4,33 @@ public class right_angle_triangle {
         int n=5;
         //its right angle traingle
         for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j <= i; j++) {
                 IO.print("* ");
             }
             IO.println(" ");
         }IO.println("Right angle traingle\n");
 
         //inverse right angle traingle
-
         for (int i = n; i >= 0; i--) {
             for (int j = i; j >= 0; j--){
                 IO.print("* ");
             }IO.println("");
-            }
+        }
             IO.println("This is Inverse of right angle traingle\n");
         
-        }
+        //hollow right angle triangle
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j==0||i==n-1||j==i) {
+                    IO.print( "* ");
+                }else{
+                    IO.print("  ");
+                } 
+            }
+            IO.println("");
+        }IO.println("Hollow Right angle traingle\n");
+    
+    }
+
+        
     }
