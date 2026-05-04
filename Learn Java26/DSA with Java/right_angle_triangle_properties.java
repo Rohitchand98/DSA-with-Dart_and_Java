c1c97e7c25aef4d85b371515e474e3b021e0d210
Rohitchand98 <rs486345@gmail.com>
2026-05-05 03:47:51 +0530

    increasing number in triangle

--- Learn Java26/DSA with Java/right_angle_triangle_properties.java
+++ Learn Java26/DSA with Java/right_angle_triangle_properties.java
@@ -8,7 +8,7 @@ public class right_angle_triangle_properties {
                 IO.print("* ");
             }
             IO.println(" ");
-        }IO.println("Right angle traingle\n");
+        }IO.println("Right angle traingle");
 
         //inverse right angle traingle
         for (int i = n; i >= 0; i--) {
@@ -16,7 +16,7 @@ public class right_angle_triangle_properties {
                 IO.print("* ");
             }IO.println("");
         }
-            IO.println("This is Inverse of right angle traingle\n");
+            IO.println("This is Inverse of right angle traingle ");
         
         //hollow right angle triangle
         for (int i = 0; i < n; i++) {
@@ -28,7 +28,7 @@ public class right_angle_triangle_properties {
                 } 
             }
             IO.println("");
-        }IO.println("Hollow Right angle traingle\n");
+        }IO.println("Hollow Right angle traingle ");
 
         //tilted right angle triangle
         for (int i = 0; i < n; i++) {
@@ -38,7 +38,7 @@ public class right_angle_triangle_properties {
             for (int k = 0; k <= i; k++) {
                 IO.print(" * ");
             }IO.println();
-        }IO.println("tilted or mirror Right angle traingle\n");
+        }IO.println("tilted or mirror Right angle traingle ");
 
         //rightangle with parameter with series of numbers
         for (int i = 0; i < n; i++) {
@@ -50,13 +50,23 @@ public class right_angle_triangle_properties {
             for (int j = i; j < n; j++) {
                 IO.print(j);
             }IO.println();
-        }IO.println("series of number with two Right angle traingle\n");
+        }IO.println("series of number with two Right angle traingle ");
 
         //Binary numbers in right angle triangle
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                IO.print(j%2);
             }IO.println();
-        }IO.println("Binary number in Right angle traingle\n");
+        }IO.println("Binary number in Right angle traingle ");
+
+        //Increasing of Numbers in right angle triangle
+        int num=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int sum=num+=1;
+               IO.print(sum+" ");
+            }IO.println();
+        }
+        IO.println("series of number with two Right angle traingle ");
     }   
     }
