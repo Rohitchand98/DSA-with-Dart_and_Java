6882b079615ddc3d2dc069744f810811fb7890e4
Rohitchand98 <rs486345@gmail.com>
2026-05-05 02:39:47 +0530

    hollow traingle is completed

--- Learn Java26/DSA with Java/rectangle_patterns.java
+++ Learn Java26/DSA with Java/rectangle_patterns.java
@@ -2,20 +2,33 @@ public class rectangle_patterns {
     void main(){
         int n=5;
 
-        //This code will print a star Square
+        //This code will print a stars of Square
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 IO.print(" * ");
             }
-            IO.println("   ");
+            IO.println("");
         }IO.println("This is a Square\n");
 
-        //This code will print a star rectangle
+        //This code will print a stars of rectangle
         for (int i = 0; i < n; i++) {
             for (int j = n/2; j < n; j++) {
                 IO.print(" * ");
             }
-            IO.println("   ");
+            IO.println("");
         }IO.println("This is a Rectangle\n");
+
+        //This code will print a star of Hollow rectangle
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i==0||j==0||i==n-1||j==n-1) {
+                    IO.print(" * ");
+                }else{
+                    IO.print("   ");
+                }
+            }IO.println("");
+            //here i have made a mistake which is i add another k forloop which increase extra weight and space
+            //if a pattern is form using 2 loop try to follow using 2
+        }IO.println("This is a Hollow Rectangle\n");
     }
 }
