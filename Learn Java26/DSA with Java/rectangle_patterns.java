ca0cff2940ceac3524e2c0b4e98bd485bf465a51
Rohitchand98 <rs486345@gmail.com>
2026-05-05 02:24:46 +0530

    squrea and rectangle pattern in java

--- Learn Java26/DSA with Java/rectangle_patterns.java
+++ Learn Java26/DSA with Java/rectangle_patterns.java
@@ -2,12 +2,20 @@ public class rectangle_patterns {
     void main(){
         int n=5;
 
-        //This code will print a star rectangle
+        //This code will print a star Square
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 IO.print(" * ");
             }
             IO.println("   ");
-        }
+        }IO.println("This is a Square\n");
+
+        //This code will print a star rectangle
+        for (int i = 0; i < n; i++) {
+            for (int j = n/2; j < n; j++) {
+                IO.print(" * ");
+            }
+            IO.println("   ");
+        }IO.println("This is a Rectangle\n");
     }
 }
