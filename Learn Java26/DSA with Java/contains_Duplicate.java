4edf02a9daf942450532a4c200901214ff353f6b
Rohitchand98 <rs486345@gmail.com>
2026-05-08 22:28:54 +0530

    optimal solution in contain duplicate

--- Learn Java26/DSA with Java/contains_Duplicate.java
+++ Learn Java26/DSA with Java/contains_Duplicate.java
@@ -1,12 +1,23 @@
 //duplicate value in array
 public boolean duplicatevalue(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1; i<nums.length; i++){
+    //Brute force Solution 
+    /*Arrays.sort(nums);
+    for(int i=1; i<nums.length; i++){
             if (nums[i-1]== nums[i]) {
                 return true;
             }
     }
+    return false;*/
+    //efficient and optical solution hashset
+    HashSet<Integer> numseen= new HashSet<>();
+        for(int num:nums){
+            if(!numseen.add(num)){
+                return true;
+            }
+        }
     return false;
+
+
 }
 
 void main() {
