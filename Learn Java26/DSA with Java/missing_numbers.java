6e191f246e811b5f9b74d6f1293709986b7215e7
Rohitchand98 <rs486345@gmail.com>
2026-05-09 04:50:32 +0530

    now optimal solution for missing numbers

--- Learn Java26/DSA with Java/missing_numbers.java
+++ Learn Java26/DSA with Java/missing_numbers.java
@@ -2,18 +2,27 @@
 public int missingNumber(int[] nums) {
     //this is brute force approach
     //its written by my own logic and codes
-    Arrays.sort(nums);
+    /* Arrays.sort(nums);
     for (int i = 0; i < nums.length; i++) {
         if (i!=nums[i]) {
             return i;
         }
     }
-    return nums.length;
+    return nums.length;*/
+    
 
-    //Now efficient method
+    //Now optimal method(using math)
+    int n=nums.length;
+    int totalvalue=n*(n+1)/2;
+    int elementinarray=0;
+    for(int num: nums){
+        elementinarray+=num;
+    }
+    return totalvalue-elementinarray;
+    
 }
 void main() {
-    int [] nums={0,1};
+    int[] nums={0,1};
     IO.println(missingNumber(nums));
 }
     
