9722bfe67b87595be89b577643b0a0dc27d65a55
Rohitchand98 <rs486345@gmail.com>
2026-05-12 08:10:27 +0530

    hashset solution

--- Learn Java26/DSA with Java/SmallerThantheCurrentNumber.java
+++ Learn Java26/DSA with Java/SmallerThantheCurrentNumber.java
@@ -2,8 +2,7 @@
 int[] smallerNumInArray(int[] nums){
 
     //This is the brute force method
-    /* */
-    int[] result=new int[nums.length];
+    /*int[] result=new int[nums.length];
     for (int i = 0; i < nums.length; i++) {
         int count=0;
         for (int j = 0; j < nums.length; j++) {
@@ -13,9 +12,25 @@ int[] smallerNumInArray(int[] nums){
         }
         result[i]=count;
     }
-    return result;
+    return result; */
+    
 
     //hashset Solution
+    int[] sortedNums = nums.clone();
+        Arrays.sort(sortedNums);
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < sortedNums.length; i++) {
+            // putIfAbsent ensures we only record the index of the FIRST occurrence 
+            map.putIfAbsent(sortedNums[i], i);
+        }
+        
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = map.get(nums[i]);
+        }
+        
+        return result;
 }
 void main() {
     int[] nums = {8,1,2,2,3};
