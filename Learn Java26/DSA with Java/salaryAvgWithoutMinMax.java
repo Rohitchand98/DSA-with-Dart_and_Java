276c25b54c61f9b5415c2f7ef5a6790357e5be07
Rohitchand98 <rs486345@gmail.com>
2026-05-14 08:05:29 +0530

    a DSA problem to solve the salary average wihout max and mid

---
+++ Learn Java26/DSA with Java/salaryAvgWithoutMinMax.java
@@ -0,0 +1,20 @@
+void main(){
+    int[] salary={4000,3000,1000,2000};
+    Arrays.sort(salary);
+    // this is brute force solution without any help
+    /* double sum=0;
+    double total=0;
+    int count=0;
+    IO.print(Arrays.toString(salary));
+    for (int check = 0; check < salary.length; check++) {
+        sum=salary[0]+salary[salary.length-1];
+        total+=salary[check];
+        count++;
+
+    }
+    double solution=(total-sum)/(count-2);
+    IO.print(solution);*/
+    
+
+    
+}
