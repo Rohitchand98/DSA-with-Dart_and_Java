827d5279149c56c1b4d9d1fb05f056bcfc10fd56
Rohitchand98 <rs486345@gmail.com>
2026-05-17 06:29:56 +0530

    min and max in list

--- Learn Java26/DSA with Java/NumberOfIsland.java
+++ Learn Java26/DSA with Java/NumberOfIsland.java
@@ -1,6 +1,7 @@
 
 //in This process we are finding island from grid of arrays
 //char is used to check the island
+//Depth first search is used in this process
 void main() {
         char[][] grid = {
             {'1', '1', '1', '1', '0'},
@@ -26,17 +27,19 @@ void main() {
 }
 private void dfs(char[][] grid, int i, int j) {
     if (i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0') {
-        return;
-    }
+        return;//Okay, I’m done here, stop and go back to the caller
+    }//If the indices are out of bounds or the cell is water ('0'), you immediately exit the method.No further recursion happens for that branch.
 
     //assign all the traversed island
     grid[i][j]='0';
 
-    //check all the sides of the island adjacents
+    //check all the sides of the island adjacents, DFS: “Consume the whole island at once” (mark it visited).
     dfs(grid, i-1,j);
     dfs(grid, i+1,j);
     dfs(grid, i,j-1);
     dfs(grid, i,j+1);
+    //Why a function can call itself
+    //This is the essence of recursive algorithms: a function invokes itself with slightly different parameters until a base condition (return;) stops the chain.
 
 }
 
--- Learn Java26/Java latest basics codes/arraylist.java
+++ Learn Java26/Java latest basics codes/arraylist.java
@@ -1,66 +1,72 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class arraylist{
-    void main(){
+public class arraylist {
+
+    void main() {
 
         /*an ArrayList is still straightforward, 
         but you now have more expressive options thanks to updates in the collections API.  */
-
         //old way
-        ArrayList<String> lst1= new ArrayList<>();
+        ArrayList<String> lst1 = new ArrayList<>();
         lst1.add("Mango");
         IO.println(lst1);
         IO.println();
 
-
         //new way
-        var lst2= new ArrayList<String>(); 
+        var lst2 = new ArrayList<String>();
         //you can remove the right String bcz its already declared.
         lst2.add("Watermallon");
         IO.println(lst2);
         IO.println();
 
         //Add multiple elements at once
-        ArrayList<String> lst= new ArrayList<>(Arrays.asList("Rohit","Tom","Hari"));
-        for(Object ele:lst){
-            IO.print(" "+ele);
+        ArrayList<String> lst = new ArrayList<>(Arrays.asList("Rohit", "Tom", "Hari"));
+        for (Object ele : lst) {
+            IO.print(" " + ele);
         }
 
         //for more than 1 datatype in list like <int, String>
-        ArrayList<Object> lst3= new ArrayList<>();
+        ArrayList<Object> lst3 = new ArrayList<>();
         lst3.add(0, "Orange");
         lst3.add(1, 2);
         for (Object obj : lst3) {
-          if(obj instanceof String s) {//Integer for ints
-              IO.println(s);
-          }else if (obj instanceof Integer i) {
-              IO.println(i);
-          }
+            if (obj instanceof String s) {//Integer for ints
+                IO.println(s);
+            } else if (obj instanceof Integer i) {
+                IO.println(i);
+            }
         }
         IO.println();
 
-
         //OR use map to seperate the interface not inside the arraylist Example Hashmap
-        Map<Integer, String > map = new HashMap<>();
+        Map<Integer, String> map = new HashMap<>();
         map.put(1, "Banana");
         map.put(2, "Apple");
         for (Map.Entry<Integer, String> m : map.entrySet()) {
-            IO.println(m.getKey()+ " "+ m.getValue());
+            IO.println(m.getKey() + " " + m.getValue());
         }
         IO.println();
 
-
         //OR use Pair
-        record Pair(int id, String name){}
-        ArrayList<Pair> lst4= new ArrayList<>();
+        record Pair(int id, String name) {
+
+        }
+        ArrayList<Pair> lst4 = new ArrayList<>();
         lst4.add(new Pair(1, "Pineapple"));
-        for( Pair p: lst4){
-            IO.println(p.id+" "+ p.name);
+        for (Pair p : lst4) {
+            IO.println(p.id + " " + p.name+"\n");
         }
 
-}
+        //Max min
+        List<Integer> nums = Arrays.asList(5, 2, 9, 1, 7);
+        System.out.println(Collections.min(nums)); // 1
+        System.out.println(Collections.max(nums)); // 9
+
+    }
 }
