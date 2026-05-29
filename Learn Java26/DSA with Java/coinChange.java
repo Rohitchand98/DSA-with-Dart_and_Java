b9cc6d9a84c5519e664cfd465fc0a10964027f6b
Rohitchand98 <rs486345@gmail.com>
2026-05-29 23:23:13 +0530

    clearing confusion in coinchange

--- Learn Java26/DSA with Java/coinChange.java
+++ Learn Java26/DSA with Java/coinChange.java
@@ -11,9 +11,14 @@ void main(){
     for (int i = 1; i <=amount; i++) {
         for (int j=0; j<coins.length; j++) {
             if (coins[j]<=i) {
+                // Update dp[i] with the minimum of:
+                // 1. Its current known minimum
+                // 2. 1 (representing using this coin) + the optimal solution for the remaining amount
+
                 dp[i]=Math.min(dp[i], dp[i-coins[j]]+1);
             }  
         }
     }
+     // If the target amount's dp value is still greater than the amount, no valid combination was ever found
     IO.print(dp[amount]>amount?-1:dp[amount]);
 }
