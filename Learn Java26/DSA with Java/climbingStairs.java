9adc452a45d9d02169bcef82c91c1de14cf49d96
Rohitchand98 <rs486345@gmail.com>
2026-05-30 21:07:18 +0530

    climbing stairs DSA

---
+++ Learn Java26/DSA with Java/climbingStairs.java
@@ -0,0 +1,15 @@
+      void main() {
+        int n=4;
+        if (n<=1) {
+            IO.print(1);
+        }
+
+        
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        IO.print(dp[n]);
+      }
